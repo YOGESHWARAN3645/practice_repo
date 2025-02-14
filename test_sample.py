1857436f0ea8147173eaad24753cd0826f451767
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2025-02-14 11:55:25 +0530

    Delete test_sample.py

--- test_sample.py
+++
@@ -1,9 +0,0 @@
-
-import pytest
-import main  # Import your main.py file
-
-def test_print_output(monkeypatch):
-    # Capture the printed output
-    captured = monkeypatch.stdout
-    main()  # Run the main function
-    assert captured == "Hi YOGESH\n"
